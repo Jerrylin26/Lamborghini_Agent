12ac514db9323e3f4d2479e6c66a49f5965f3506
Jerry Lin <jerry8g@gmail.com>
2025-10-09 22:57:45 +0800

    start OrderController and AccountController

--- c#/Lamborghini/Controllers/OrderController.cs
+++ c#/Lamborghini/Controllers/OrderController.cs
@@ -1,10 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Lamborghini.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Text.Json;
+
 
 namespace Lamborghini.Controllers
 {
     public class OrderController : Controller
     {
         public IActionResult Checkout()
+        {
+            List<CartItem> cart;
+            if (Request.Cookies.TryGetValue("LamboCart", out var cookieValue))
+            {
+                cart = JsonSerializer.Deserialize<List<CartItem>>(cookieValue) ?? new List<CartItem>();
+
+            }
+            else
+            {
+                cart = new List<CartItem>();
+            }
+
+            DBmanager db = new DBmanager();
+            double totalPrice = 0;
+            foreach (var item in cart)
+            {
+                var product = db.getDetail(item.ProductId)[0]; // 只取第一個產品,由於都是同個商品
+                totalPrice += product.Price * item.Quantity;
+
+            }
+
+            return View(totalPrice);
+
+        }
+        public IActionResult Confirm()
         {
             return View();
         }
